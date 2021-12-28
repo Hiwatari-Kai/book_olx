1ff213a104ea62282d2c131f1f50f2f59254361c
Hiwatari-Kai <75009118+Hiwatari-Kai@users.noreply.github.com>
2021-12-28 18:51:09 +0530

    created index.js in api

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,3 +1,5 @@
+import connectDB from '../../utils/dbConnect';
+import Book from '../../models/Book';
 export default async (req, res) => {
     const { method } = req;
   
@@ -5,4 +7,20 @@ export default async (req, res) => {
     if (method === "OPTIONS") {
       return res.status(200).send("ok");
     }
+    switch(method){
+        case 'POST':
+            try {
+                const book = await Book.create(req.body);
+                res.status(201).json({success :true , data :book});
+            } catch (error) {
+                res.status(400).json({success :false});
+            }
+            break;
+        default :
+            res.status(400).json({success :false});
+            break;
+
   };
+}
+
+export default connectDB(handler)
