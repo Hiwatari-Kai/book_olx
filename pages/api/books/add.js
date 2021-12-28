466618634f02340e773ed08094ce61837ce483e0
Hiwatari-Kai <75009118+Hiwatari-Kai@users.noreply.github.com>
2021-12-28 18:42:04 +0530

    another fix

--- pages/api/books/add.js
+++ pages/api/books/add.js
@@ -1,30 +1,8 @@
 import connectDB from '../../../utils/dbConnect';
 import Book from '../../../models/Book';
-import Cors from 'cors'
-
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ['GET', 'HEAD','POST'],
-})
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-
-      return resolve(result)
-    })
-  })
-}
-
-
 
 const handler = async (req, res) => {
-    await runMiddleware(req, res, cors)
+    
     const {method} = req;
     
     switch(method){
